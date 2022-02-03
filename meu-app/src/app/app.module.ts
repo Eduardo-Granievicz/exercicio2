289c52a5dddb9803f077704a931b92082bb93603
eduardo-granievicz <eduardo.moreno@bandtec.com>
2022-02-03 11:32:51 -0300

    Exercicio 2

---
+++ meu-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { FooterComponent } from './componets/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
